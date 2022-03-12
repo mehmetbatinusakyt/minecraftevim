ea0283fb3f90bd6d568039f5b54d1814dbf22f1a
mehmetbatinusakyt <82940666+mehmetbatinusakyt@users.noreply.github.com>
2022-03-12 23:34:15 +0300

    Update minecraftevim.sh

--- minecraftevim.sh
+++ minecraftevim.sh
@@ -11,7 +11,7 @@ echo ''
 echo ''
 if [ ! -f kuruldu ]; then
 echo 'İstediğiniz server sürümünü seçiniz.'
-echo '1) 1.8.8 Paper 2) 1.12.2 Paper '
+echo '1) 1.8.8 Paper 2) 1.12.2 Paper 3) 1.14.4 Paper 4) 1.16.5 Paper'
 echo ''
 read surum
 
@@ -19,15 +19,29 @@ read surum
 
 if [ ! -f server.jar ]; then
 if [ surum=="1" ]; then
-wget https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/443/downloads/paper-1.8.8-443.jar
+curl https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/443/downloads/paper-1.8.8-443.jar > paper-1.8.8-443.jar
 mv paper-1.8.8-443.jar server.jar
 touch kuruldu
 fi
 fi
 if [ ! -f server.jar ]; then
 if [ surum=="2" ]; then
-wget https://papermc.io/api/v2/projects/paper/versions/1.12.2/builds/1618/downloads/paper-1.12.2-1618.jar
+wget https://papermc.io/api/v2/projects/paper/versions/1.12.2/builds/1618/downloads/paper-1.12.2-1618.jar > paper-1.12.2-1618.jar
 mv paper-1.12.2-1618.jar server.jar
+touch kuruldu
+fi
+fi
+
+if [ ! -f server.jar ]; then
+if [ surum=="3" ]; then
+wget https://papermc.io/api/v2/projects/paper/versions/1.14.4/builds/243/downloads/paper-1.14.4-243.jar > server.jar
+touch kuruldu
+fi
+fi
+
+if [ ! -f server.jar ]; then
+if [ surum=="4" ]; then
+wget https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/790/downloads/paper-1.16.5-790.jar > server.jar
 touch kuruldu
 fi
 fi
@@ -35,7 +49,7 @@ fi
 fi
 
 if [ -f kuruldu ]; then
-java -Xms128M -Xmx{{SERVER_MEMORY}}M -jar {{SERVER_JARFILE}}
+java -Xms128M -Xmx1024M -jar server.jar
 fi
 
 if [ ! -f server.properties ]; then
